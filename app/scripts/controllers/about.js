1905936dba4ec273b87d5d5117eb344f8eb14cbd
manideep-1 <laxmishettymani@gmail.com>
2021-02-19 14:25:25 +0530

    angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name basicappApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the basicappApp
+ */
+angular.module('basicappApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name basicappApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the basicappApp
+ */
+angular.module('basicappApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
